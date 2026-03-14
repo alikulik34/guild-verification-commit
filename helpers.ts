1b2d5fb903fefca044c36f048e87a07e7b37cff2
alikulik34 <166433829+alikulik34@users.noreply.github.com>
2026-03-14 21:33:02 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T18:32:57.772Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-14T18:32:58.823Z
+Commit: Fix broken links in documentation
