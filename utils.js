6660c87aaaa394fad7ee8210e1385673a03f9151
alikulik34 <166433829+alikulik34@users.noreply.github.com>
2026-03-14 21:36:28 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T18:33:05.187Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-14T18:36:25.608Z
+Commit: Fix broken links in documentation
