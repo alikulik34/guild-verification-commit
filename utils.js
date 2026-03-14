522843cc7d25552fa2f38531b84d25ae7789a891
alikulik34 <166433829+alikulik34@users.noreply.github.com>
2026-03-14 23:07:07 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:07:05.391Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-14T20:07:07.307Z
+Commit: Fix broken links in documentation
