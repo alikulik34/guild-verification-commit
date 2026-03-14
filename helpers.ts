2d609c0b753d3b4be86fd257d6d13e31321ea270
alikulik34 <166433829+alikulik34@users.noreply.github.com>
2026-03-14 21:36:22 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T18:36:16.453Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T18:36:19.597Z
+Commit: Patch API endpoint error handling
